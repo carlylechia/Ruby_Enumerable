5f4330413acf8ea1fc12700e6a91f00063d123ab
frank1738 <franklineosoro08@gmail.com>
2022-07-14 16:04:17 +0300

    create a list class

---
+++ class.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class MyList
+  def initialize(*list)
+    @list = list
+  end
+
+  include MyEnumerable
+
+  def each
+    i = 0
+    while i < @list.length
+      puts "That is #{i}"
+      yield @list[i]
+      i += 1
+    end
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+
+puts(list.any? { |e| e == 5 })
+
+p list.filter(&:even?)
+p list.filter(&:odd?)
